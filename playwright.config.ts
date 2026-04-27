91b1631ae73ed75fe7c84b7caf9c4fde29be048a
pradeephbtu@gmail.com <pradeephbtu@gmail.com>
2026-04-28 00:20:00 +0530

    Updated playwright config ts file for allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,18 +9,20 @@ export default defineConfig({
   retries: 1,
 
   reporter: [
-    ['html', { outputFolder: 'reports/html' }],
-    ['list'],
-    ['allure-playwright']
+    ['list'], // console output
+    ['html', { outputFolder: 'reports/html', open: 'never' }], // HTML report
+    ['allure-playwright'] // Allure report
   ],
-  
 
   use: {
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
 
-    //  SAFE + COMPANY STANDARD
+    // Dynamic headless (best practice)
+    headless: process.env.CI ? true : false,
+
+    // Base URL
     baseURL: ENV.BASE_URL || 'https://www.saucedemo.com'
   }
 });
