f1f38cd6aedb51dbfe6bae348e34a20a94a4c939
pradeephbtu@gmail.com <pradeephbtu@gmail.com>
2026-05-02 14:47:24 +0530

    Updated the tsConfigJson file for import purpose

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,8 +1,9 @@
 
 import { test, expect } from '@playwright/test';
 
-import { LoginActions } from '../../actions/LoginActions';
-import { getLoginData } from '../../test-data/testDataProvider';
+import { LoginActions } from '@actions/LoginActions';
+import { getLoginData } from '@testdata/testDataProvider';
+import '@hooks/testHooks';
 
 test('@smoke Login Test1', async ({ page }) => {
 
@@ -17,15 +18,10 @@ test('@smoke Login Test1', async ({ page }) => {
 
     await expect(page).toHaveURL(/inventory/);
 
-    await test.info().attach("screenshot", {
-        body: await page.screenshot(),
-        contentType: "image/png"
-    });
-
 
 });
 
-test('Login Test 2', async ({ page }) => {
+test('@smoke Login Test 2', async ({ page }) => {
 
     console.log("BASE URL:", test.info().project.use.baseURL);
 
@@ -34,12 +30,14 @@ test('Login Test 2', async ({ page }) => {
 
     await page.goto('/');
     await login.login(data.username, data.password);
-    await expect(page).toHaveURL(/inventory/);
+    
+     expect(true).toBe(false);
+    //await expect(page).toHaveURL(/inventory/);
 
 
 });
 
-test('Login Test3', async ({ page }) => {
+test('@smoke Login Test3', async ({ page }) => {
 
     const data = getLoginData();
     const loginObj = new LoginActions(page);
